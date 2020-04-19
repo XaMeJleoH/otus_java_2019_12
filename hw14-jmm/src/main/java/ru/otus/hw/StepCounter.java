5112df30332b576f4037c97005d3c479f679e6c1
Aleksey <xamejleoh@list.ru>
2020-04-19 18:14:59 +0300

    hw14 - Finished

--- hw14-jmm/src/main/java/ru/otus/hw/StepCounter.java
+++ hw14-jmm/src/main/java/ru/otus/hw/StepCounter.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class StepCounter {
@@ -19,22 +18,46 @@ public class StepCounter {
 
     private void loop() {
         for (int i = 0; i < LIMIT; i++) {
-            System.out.println("i= " + i);
-            forwardStep();
+            isStep(true);
+        }
+
+        for (int i = 1; i < LIMIT; i++) {
+            isStep(false);
         }
     }
 
-    private void forwardStep() {
+    private void isStep(boolean forward) {
         if (onSomeSide.compareAndSet(true, false)) {
-            leftCount++;
-            log.info("L leg, count= {}", leftCount);
-            sleep(1);
+            synchronized (this) {
+                leftIncrOrDecr(forward);
+                log.info("L leg, count= {}", leftCount);
+                sleep(2);
+            }
         } else if (onSomeSide.compareAndSet(false, true)) {
-            rightCount++;
-            log.info("R leg, count= {}", rightCount);
-            sleep(2);
+            synchronized (this) {
+                rightIncrOrDecr(forward);
+                log.info("R leg, count= {}", rightCount);
+                sleep(1);
+            }
         }
     }
+
+    private void rightIncrOrDecr(boolean forward) {
+        if (forward) {
+            rightCount++;
+        } else {
+            rightCount--;
+        }
+    }
+
+    private void leftIncrOrDecr(boolean forward) {
+        if (forward) {
+            leftCount++;
+        } else {
+            leftCount--;
+        }
+    }
+
 
     private void go() throws InterruptedException {
         Thread thread1 = new Thread(this::loop);
@@ -43,10 +66,10 @@ public class StepCounter {
         thread1.start();
         thread2.start();
 
-        System.out.println("A baby start to walk");
+        System.out.println("Baby start to walk");
         thread1.join();
         thread2.join();
-        System.out.println("A baby finished walk");
+        System.out.println("Baby finished walk");
     }
 
     private static void sleep(int duration) {
